df311080f4ac6be6c27f2229d4d8759e446dbb21
imthepooh <i.m.thepooh@gmail.com>
2020-01-03 11:37:43 -0800

    Add clarity to the bot with comments in code

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import urllib
 import os
 import telegram
 
+# declare the bot
 bot = telegram.Bot(token=os.environ['TOKEN'])
+# set the base url for scrapping
 url = 'https://search.f-droid.org/?lang=en&page='
 
 def appsearch(request):
@@ -17,13 +19,17 @@ def appsearch(request):
         update = telegram.Update.de_json(request.get_json(request),bot)
         chat_id = update.message.chat.id
         rec_msg = update.message.text.split(' ')
+        # start handler for the bot
         if rec_msg[0] == '/start':
             res_text = 'Hello, I am a bot. \nI can search apps on f-droid based on search keyword. \nPlease check /help for details'
             bot.send_message(chat_id=chat_id,text=res_text)
+        # help handler for the bot
         elif rec_msg[0] == '/help':
             res_text = 'Enter the search string as /s<space><search string> to get a list of all applications from f-droid containing the string'
             bot.send_message(chat_id=chat_id,text=res_text)
+        # search handler starting with /s command 
         elif rec_msg[0] == '/s':
+            # check for valid search string
             if len(rec_msg) < 2:
                 res_text = 'Please enter a valid search string. Use /help for details'
                 bot.send_message(chat_id=chat_id,text=res_text)
@@ -31,42 +37,35 @@ def appsearch(request):
                 search_app = urllib.parse.quote_plus(' '.join(rec_msg[1:]))
                 i = 1
                 url_set = url + str(i) + '&q=' + search_app
-                #print('log: Getting data for url: ',url_set)
                 s_page = requests.get(url_set)
+                # fetch the search results
                 soup = BeautifulSoup(s_page.content,'html.parser')
-                #print('log: retrieved the page')
                 num = 0
+                # check if the search results are multipaged
                 for nums in soup.find_all('span',class_='step-links'):
-                    #print('log: nums value: ', nums.get_text())
                     try:
                         num = int(nums.get_text())
                     except:
                         num_str = nums.get_text()
-                #print('log: checking for page numbers')
+                # set the number of pages to be scrapped
                 i = 1 if num == 0 else num
-                #print(f'log: iterating for {i} times')
                 for n in range(1,i+1):
                     if n > 1:
-                        #print('log: fetching page: ',n)
                         url_set = url + str(n) + '&q=' + search_app
                         s_page = requests.get(url_set)
                         soup = BeautifulSoup(s_page.content,'html.parser')
-                    #print('log: finding the results')
+                    # get app name, details and url 
                     for app_h in soup.find_all('a',class_='package-header'):
-                        # print(app_h)
                         app_url = app_h['href'].strip()
-                        # print(app_url)
                         
                         app_n = app_h.find('h4',class_='package-name')
                         app_name = app_n.get_text().strip()
-                        # print(app_name)
 
                         app_s = app_h.find('span',class_='package-summary')
                         app_sum = app_s.get_text().strip()
-                        # print(app_sum)
+                        # prepare result string
                         fin_text += 'name: ' + app_name + '\n'+ 'desc: ' + app_sum+'\n'+ 'url: ' + app_url +'\n\n'
-                        #fin_list.append(fin_text)
-                #if len(fin_list) == 0:
+                # respond with message if no results found
                 if fin_text == '':
                     res_text = 'Sorry, No results found'
                     bot.send_message(chat_id=chat_id,text=res_text)
@@ -75,6 +74,7 @@ def appsearch(request):
                     for text_msg in fin_list:
                         bot.send_message(chat_id=chat_id,text=text_msg)
         else:
+            # handle invalid command sent to bot
             res_text = 'Invalid command, please use /help for details'
             bot.send_message(chat_id=chat_id,text=res_text)
     return f'ok'
